20f792940b49fe4aadf328b6f841f27946287963
Anusha <anushasaraf1306@gmail.com>
2023-06-28 11:43:54 -0500

    update

--- src/lab5.ino
+++ src/lab5.ino
@@ -1,32 +1,28 @@
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
-
 // Libraries
 #include <math.h>
 
 // Pins
-const int lightSensorPin = A0;  // Analog pin for light sensor
-const int ledPin = D2;          // Digital pin for LED
+const int lightSensorPin = A5; // Analog pin for light sensor
+const int ledPin = D5;         // Digital pin for LED
 
 // Constants
-const int minSensorValue = 0;   // Minimum value of light sensor reading
-const int maxSensorValue = 4095;// Maximum value of light sensor reading
-const int minBrightness = 0;    // Minimum brightness value for LED
-const int maxBrightness = 255;  // Maximum brightness value for LED
+const int minSensorValue = 0;    // Minimum value of light sensor reading
+const int maxSensorValue = 4095; // Maximum value of light sensor reading
+const int minBrightness = 0;     // Minimum brightness value for LED
+const int maxBrightness = 255;   // Maximum brightness value for LED
 
 // Variables
-int sensorValue = 0; 
+int sensorValue = 0;
 int ledBrightness = 0;
 
 // Function to map a value from one range to another
-double mapValue(double value, double fromLow, double fromHigh, double toLow, double toHigh) {
+double mapValue(double value, double fromLow, double fromHigh, double toLow, double toHigh)
+{
   return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
 }
-
-
-
-
 
 /*
  * Project lab5
@@ -36,13 +32,15 @@ double mapValue(double value, double fromLow, double fromHigh, double toLow, dou
  */
 
 // setup() runs once, when the device is first turned on.
-void setup() {
-  pinMode(ledPin, OUTPUT);        // Set LED pin as output
-  Particle.variable("brightness", ledBrightness);  // Expose brightness variable
+void setup()
+{
+  pinMode(ledPin, OUTPUT);                        // Set LED pin as output
+  Particle.variable("brightness", ledBrightness); // Expose brightness variable
 }
 
 // loop() runs over and over again, as quickly as it can execute.
-void loop() {
+void loop()
+{
   // Read the light sensor value
   sensorValue = analogRead(lightSensorPin);
 
